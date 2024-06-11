ab36a3101eda92754e97af592ff22aa4fb84d38d
Iskander Romdhane <skander20034@gmail.com>
2024-06-11 19:29:25 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -4,32 +4,33 @@
         <title>Burger Code</title>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
         <link href='http://fonts.googleapis.com/css?family=Holtwood+One+SC' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
         <link rel="stylesheet" href="css/styles.css">
     </head>
-    
-    
     <body>
         <div class="container site">
-            <h1 class="text-logo"><span class="glyphicon glyphicon-cutlery"></span> Burger Code <span class="glyphicon glyphicon-cutlery"></span></h1>
+           
+            <h1 class="text-logo"><span class="bi-shop"></span> Burger Code <span class="bi-shop"></span></h1>
+            
             <?php
 				require 'admin/database.php';
 			 
                 echo '<nav>
-                        <ul class="nav nav-pills">';
+                        <ul class="nav nav-pills" role="tablist">';
 
                 $db = Database::connect();
                 $statement = $db->query('SELECT * FROM categories');
                 $categories = $statement->fetchAll();
-                foreach ($categories as $category) 
-                {
-                    if($category['id'] == '1')
-                        echo '<li role="presentation" class="active"><a href="#'. $category['id'] . '" data-toggle="tab">' . $category['name'] . '</a></li>';
-                    else
-                        echo '<li role="presentation"><a href="#'. $category['id'] . '" data-toggle="tab">' . $category['name'] . '</a></li>';
+                foreach ($categories as $category) {
+                    if($category['id'] == '1') {
+                        echo '<li class="nav-item" role="presentation"><a class="nav-link active" data-bs-toggle="pill" data-bs-target="#tab'. $category['id'] . '" role="tab">' . $category['name'] . '</a></li>';
+                    } else {
+                        echo '<li class="nav-item" role="presentation"><a class="nav-link" data-bs-toggle="pill" data-bs-target="#tab'. $category['id'] . '" role="tab">' . $category['name'] . '</a></li>';
+                    }
                 }
 
                 echo    '</ul>
@@ -37,27 +38,26 @@
 
                 echo '<div class="tab-content">';
 
-                foreach ($categories as $category) 
-                {
-                    if($category['id'] == '1')
-                        echo '<div class="tab-pane active" id="' . $category['id'] .'">';
-                    else
-                        echo '<div class="tab-pane" id="' . $category['id'] .'">';
+                foreach ($categories as $category) {
+                    if($category['id'] == '1') {
+                        echo '<div class="tab-pane active" id="tab' . $category['id'] .'" role="tabpanel">';
+                    } else {
+                        echo '<div class="tab-pane" id="tab' . $category['id'] .'" role="tabpanel">';
+                    }
                     
                     echo '<div class="row">';
                     
                     $statement = $db->prepare('SELECT * FROM items WHERE items.category = ?');
                     $statement->execute(array($category['id']));
-                    while ($item = $statement->fetch()) 
-                    {
-                        echo '<div class="col-sm-6 col-md-4">
-                                <div class="thumbnail">
-                                    <img src="images/' . $item['image'] . '" alt="...">
+                    while ($item = $statement->fetch()) {
+                        echo '<div class="col-md-6 col-lg-4">
+                                <div class="img-thumbnail">
+                                    <img src="images/' . $item['image'] . '" class="img-fluid" alt="...">
                                     <div class="price">' . number_format($item['price'], 2, '.', ''). ' €</div>
                                     <div class="caption">
                                         <h4>' . $item['name'] . '</h4>
                                         <p>' . $item['description'] . '</p>
-                                        <a href="#" class="btn btn-order" role="button"><span class="glyphicon glyphicon-shopping-cart"></span> Commander</a>
+                                        <a href="#" class="btn btn-order" role="button"><span class="bi-cart-fill"></span> Commander</a>
                                     </div>
                                 </div>
                             </div>';
@@ -69,7 +69,7 @@
                 Database::disconnect();
                 echo  '</div>';
             ?>
+
         </div>
     </body>
 </html>
-
